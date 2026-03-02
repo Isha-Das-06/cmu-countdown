8742c086b68200a41d392f2b7e6f97d9caaffb2c
Isha <ishadas2006@gmail.com>
2026-03-02 09:07:39 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-cyan': '#00f3ff',
+        'neon-purple': '#bc13fe'
+      },
+      fontFamily: {
+        sans: ['Space Grotesk', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        display: ['Orbitron', 'ui-sans-serif', 'system-ui', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
